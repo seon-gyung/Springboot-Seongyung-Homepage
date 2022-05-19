e17deb166933f697990a21d34801ff3298410d84
seongyung <282203@naver.com>
2022-05-19 16:41:55 +0900

    initial commit

---
+++ src/main/java/site/metacoding/seongyung/SeongyungApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.seongyung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeongyungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeongyungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/seongyung/SeongyungApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.seongyung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeongyungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
